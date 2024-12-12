928865ffa8c305f1758680bf87a26ee5b73b9ec6
蘭丸 千歳 <230063@jc-21.jp>
2024-12-12 11:37:33 +0900

    Lスティック完成

--- Operation/TestScene.cpp
+++ Operation/TestScene.cpp
@@ -2,22 +2,40 @@
 
 TestScene::TestScene(GameObject* parent)
 {
-
 }
 void TestScene::Initialize()
 {
-	// ‚R‚c‹óŠÔã‚Éü•ª‚ð•`‰æ‚·‚é
-	DrawLine3D(VGet(100.0f, 100.0f, 0.0f), VGet(300.0f, 200.0f, 0.0f), GetColor(255, 255, 0));
+
 }
 
 void TestScene::Update() 
 {
-
+	// ƒJƒƒ‰‚ÌˆÊ’u‚Æ’Ž‹“_‚ðƒZƒbƒg
+	//SetCameraPositionAndTarget_UpVecY(VGet(x+1, y-500, z),VGet(x,y,z));
+	// “ü—Íó‘Ô‚ðŽæ“¾
+	GetJoypadXInputState(DX_INPUT_PAD1, &input);
+	if (input.ThumbLX >= 10000)
+	{
+		x += 25;
+	};
+	if (input.ThumbLY >= 10000)
+	{
+		y += 25;
+	};
+	if (input.ThumbLX <= -10000)
+	{
+		x -= 25;
+	};
+	if (input.ThumbLY <= -10000)
+	{
+		y -= 25;
+	};
 }
 
 void TestScene::Draw()
 {
-
+	// ‚R‚c‹óŠÔã‚Éü•ª‚ð•`‰æ‚·‚é
+	DrawSphere3D(VGet(x, y, z), 80.0f, 32, GetColor(255, 0, 0), GetColor(255, 255, 255), TRUE);
 }
 
 void TestScene::Release()
--- Operation/TestScene.h
+++ Operation/TestScene.h
@@ -3,6 +3,9 @@
 #include <DxLib.h>
 class TestScene:public GameObject
 {public:
+	int x = 0.0f, y = 0.0f, z = 0.0f;
+	XINPUT_STATE input;
+	VECTOR Position = VGet(x, y, z);
 	TestScene(GameObject* parent);
 	void Initialize() override;
 	void Update() override;
